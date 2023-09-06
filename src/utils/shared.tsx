16f45f9106509cbcd1fc7a2c071dc4e953b6ff51
beny <benyyoulove@gmail.com>
2023-09-06 22:22:32 +0300

    none

--- src/utils/shared.tsx
+++ src/utils/shared.tsx
@@ -1,17 +1,17 @@
 import styled from "styled-components/native";
 import { Dimensions, PixelRatio } from "react-native";
-import { AppColors } from "./colors"; // Assuming you have a 'colors.js' file for color definitions.
+import { AppColors } from "./colors";
 
 // Define a styled component called 'Container'
 export const Container = styled.View`
     align-items: center;
     flex: 1;
-    background-color: ${AppColors.background}; // Use the 'background' color from the 'colors' module.
+    background-color: ${AppColors.background}; 
 `;
 
 // Get the width and height of the screen using Dimensions module
-const dpWidth = Dimensions.get("screen").width; // Width of the screen in device-independent pixels (DP).
-const dpHeight = Dimensions.get("screen").height; // Height of the screen in device-independent pixels (DP).
+const dpWidth = Dimensions.get("screen").width;
+const dpHeight = Dimensions.get("screen").height;
 
 // Calculate and export the screen width and height rounded to the nearest pixel using PixelRatio.
 export const screenWidth = PixelRatio.roundToNearestPixel(dpWidth);
